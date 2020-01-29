adfaf32af8bb4c3a0f3e0a0390ba237b87801aa3
Will Taylor <w1ll100@hotmail.co.uk>
2020-01-29 20:09:32 +0000

    Refactor input handling

--- p2pgo/interface.py
+++ p2pgo/interface.py
@@ -1,15 +1,40 @@
-from typing import Tuple, List
+from typing import Tuple, List, TYPE_CHECKING, Optional
 
 from raylib.static import *
 from game import Colour
 
 from game import Game
 
+if TYPE_CHECKING:
+    import socket
+
+
+class Interface:
+    def __init__(self, gui: 'GUI', conn: 'socket.socket'):
+        self.gui = gui
+        self.conn = conn
+
+    def handle_inputs(self, game):
+        if game.current_player == game.player_colour:
+            click = self.gui.grid_was_clicked()
+            if click:
+                x, y = click
+                game.play(x, y)
+                self.conn.send(f"{x},{y}".encode('ascii'))
+        else:
+            try:
+                msg = self.conn.recv(1024)
+                x, y, = [int(coord) for coord in msg.decode('ascii').split(",")]
+                game.play(x, y)
+            except BlockingIOError:
+                # Thrown if nothing has been received
+                return
+        # Ghost
+
+
 # Assumption being made that grid is square in lots of places
-
-
 class GUI:
-    def __init__(self, screen_width: int, screen_height: int, grid_dims: Tuple[int, int], conn):
+    def __init__(self, screen_width: int, screen_height: int, grid_dims: Tuple[int, int]):
         self.grid_dims = grid_dims
         smaller_screen_dim = min(screen_width, screen_height)
         self.edge_length: float = smaller_screen_dim * 9 / 10
@@ -26,19 +51,10 @@ class GUI:
             (self.grid_origin[0], self.grid_origin[1] + int(y * grid_spacing_y)),
             (self.grid_origin[0] + self.edge_length, self.grid_origin[1] + int(y * grid_spacing_y))]
             for y in range(grid_dims[1])]
-        # No the GUI should not have a reference to the socket
-        # Yes I want something fast before I go to bed
-        self.conn = conn
 
-    def handle_inputs(self, game: Game):
-        if game.my_turn and IsMouseButtonReleased(MOUSE_LEFT_BUTTON):
-            x, y = self._world_to_grid(GetMouseX(), GetMouseY())
-            if x is None or y is None:
-                return
-
-            if game.is_valid_move(x, y):
-                game.play(x, y)
-                self.conn.send(f"{x},{y}".encode('ascii'))
+    def grid_was_clicked(self) -> Optional[Tuple[int, int]]:
+        if IsMouseButtonReleased(MOUSE_LEFT_BUTTON):
+            return self._world_to_grid(GetMouseX(), GetMouseY())
 
     def draw(self, game: Game):
         BeginDrawing()
@@ -58,19 +74,19 @@ class GUI:
                 DrawCircleV(self._grid_to_world(x, y), 15, colour)
         EndDrawing()
 
-    def _world_to_grid(self, wx: float, wy: float):
+    def _world_to_grid(self, wx: float, wy: float) -> Optional[Tuple[int, int]]:
         offset = wx - self.grid_origin[0], wy - self.grid_origin[1]
         x = (self.grid_dims[0] - 1) * offset[0] / self.edge_length
         y = (self.grid_dims[1] - 1) * offset[1] / self.edge_length
 
         def too_far(fractional):
-            return abs(fractional - round(fractional)) > 0.2
+            return abs(fractional - round(fractional)) > 0.3
 
         if too_far(x) or too_far(y):
-            return None, None
+            return None
 
         return round(x), round(y)
 
-    def _grid_to_world(self, x: int, y: int):
-        return [self.grid_origin[0] + self.edge_length * (x / (self.grid_dims[0] - 1)),
-                self.grid_origin[1] + self.edge_length * (y / (self.grid_dims[1] - 1))]
+    def _grid_to_world(self, x: int, y: int) -> Tuple[float, float]:
+        return (self.grid_origin[0] + self.edge_length * (x / (self.grid_dims[0] - 1)),
+                self.grid_origin[1] + self.edge_length * (y / (self.grid_dims[1] - 1)))
--- p2pgo/main.py
+++ p2pgo/main.py
@@ -2,35 +2,26 @@ import socket
 
 from raylib.static import *
 
-from game import Game
-from drawing import GUI
+from game import Game, Colour
+from interface import GUI, Interface
 
-SCREEN_WIDTH, SCREEN_HEIGHT = 1440, 900
+SCREEN_WIDTH, SCREEN_HEIGHT = 800, 600
 
 
 def run(net_conn: socket.socket, player_colour):
     net_conn.setblocking(False)
     grid_dims = grid_width, grid_height = 19, 19
     game = Game(grid_width, grid_height, player_colour)
-    # No the GUI should not have a reference to the socket
-    # Yes I want something fast before I go to bed
-    gui = GUI(SCREEN_WIDTH, SCREEN_HEIGHT, grid_dims, net_conn)
+    gui = GUI(SCREEN_WIDTH, SCREEN_HEIGHT, grid_dims)
+    interface = Interface(gui, net_conn)
 
-    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, b"Go")
-    SetWindowTitle(b"This can be changed (to show status? does show when alt-tabbing)")
+    title = f"Go | You play {'white' if player_colour == Colour.WHITE else 'black'}"
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, title.encode('ascii'))
     SetTargetFPS(60)
 
     while not WindowShouldClose():
-        gui.handle_inputs(game)
-        handle_network_input(net_conn, game)
+        interface.handle_inputs(game)
         gui.draw(game)
+    net_conn.close()
     CloseWindow()
 
-
-def handle_network_input(net_conn, game):
-    try:
-        msg = net_conn.recv(1024)
-        x, y, = [int(coord) for coord in msg.decode('ascii').split(",")]
-        game.play(x, y)
-    except BlockingIOError:
-        return
